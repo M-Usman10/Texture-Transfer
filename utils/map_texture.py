0d9ff47e4ce4c89598410732a66e6a010a3cebac
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-01-31 17:02:06 +0500

    Otmized texture transfer function

--- utils/map_texture.py
+++ utils/map_texture.py
@@ -21,36 +21,15 @@ class MapTexture:
 
     def transfer_texture(self, im, IUV):
         TextureIm = self.TextureIm
-        U = IUV[:, :, 1]
-        V = IUV[:, :, 2]
-
-        R_im = np.zeros(U.shape)
-        G_im = np.zeros(U.shape)
-        B_im = np.zeros(U.shape)
-        ###
+        generated_image = im.copy()
+        print(generated_image.max())
         for PartInd in range(1, 25):  ## Set to xrange(1,23) to ignore the face part.
             tex = TextureIm[PartInd - 1, :, :, :].squeeze()  # get texture for each part.
-            #####
-            R = tex[:, :, 0]
-            G = tex[:, :, 1]
-            B = tex[:, :, 2]
-            ###############
-            x, y = np.where(IUV[:, :, 0] == PartInd)
-            u_current_points = U[x, y]  # Pixels that belong to this specific part.
-            v_current_points = V[x, y]
-            ##
-            r_current_points = R[((255 - v_current_points) * 199. / 255.).astype(int), (
-                        u_current_points * 199. / 255.).astype(int)] * 255
-            g_current_points = G[((255 - v_current_points) * 199. / 255.).astype(int), (
-                        u_current_points * 199. / 255.).astype(int)] * 255
-            b_current_points = B[((255 - v_current_points) * 199. / 255.).astype(int), (
-                        u_current_points * 199. / 255.).astype(int)] * 255
-            ##  Get the RGB values from the texture images.
-            R_im[IUV[:, :, 0] == PartInd] = r_current_points
-            G_im[IUV[:, :, 0] == PartInd] = g_current_points
-            B_im[IUV[:, :, 0] == PartInd] = b_current_points
-        generated_image = np.concatenate((B_im[:, :, np.newaxis], G_im[:, :, np.newaxis], R_im[:, :, np.newaxis]),
-                                         axis=2).astype(np.uint8)
-        BG_MASK = generated_image == 0
-        generated_image[BG_MASK] = im[BG_MASK]  ## Set the BG as the old image.
+            u_current_points = IUV[..., 1][IUV[:, :, 0] == PartInd]  # Pixels that belong to this specific part.
+            v_current_points = IUV[..., 2][IUV[:, :, 0] == PartInd]
+            mask = ((255 - v_current_points) * 199. / 255.).astype(int), (
+                    u_current_points * 199. / 255.).astype(int)
+            tex_to_rep = tex[mask][..., ::-1]
+            print(tex_to_rep.max())
+            generated_image[IUV[:, :, 0] == PartInd] = (tex_to_rep * 255).astype(np.uint8)
         return generated_image
