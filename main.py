3db171ed95127611d047c31cd5674c28121d76f4
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-02-02 12:22:04 +0500

    Refactored

--- main.py
+++ main.py
@@ -9,32 +9,36 @@ from flask import url_for, send_from_directory, request
 from werkzeug import secure_filename
 import skimage.io as io
 import cv2
-
-
 config = load_config(r'configs.yaml')
 map_t = MapTexture(config)
 app = make_flask_app(config)
+
+
+def process_video(saved_path,video_name):
+    os.system(
+        "sudo nvidia-docker run --rm -v {}:/denseposedata -t densepose:c2-cuda9-cudnn7-wdata python2 tools/infer"
+        "_simple.py --cfg configs/DensePose_ResNet101_FPN_s1x-e2e.yaml --output-dir DensePoseData/output_results/"
+        " --image-ext jpg --wts DensePoseData/weights/weights.pkl DensePoseData/input_imgs/{}".format(
+            config['inference_dir'], video_name))
+    im = cv2.imread(saved_path)
+    iuv = cv2.imread(os.path.join(config['output_dir'], video_name[:-4] + '_IUV.png'))
+    out = map_t.transfer_texture(im, iuv)
+    result_save_file = os.path.join(app.config['UPLOAD_FOLDER'], "texture_result.jpg")
+    io.imsave(result_save_file, out[..., ::-1])
+
+
 @app.route('/', methods = ['POST'])
 def api_root():
-    if request.method == 'POST' and request.files['image']:
+    if request.method == 'POST' and request.files['video']:
         app.logger.info(app.config['UPLOAD_FOLDER'])
-        img = request.files['image']
-        img_name = secure_filename(img.filename)
+        video = request.files['video']
+        video_name = secure_filename(video.filename)
         create_new_folder(app.config['UPLOAD_FOLDER'])
-        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], video_name)
         app.logger.info("saving {}".format(saved_path))
-        img.save(saved_path)
-        im=cv2.imread(saved_path)
-        os.system(
-            "sudo nvidia-docker run --rm -v {}:/denseposedata -t densepose:c2-cuda9-cudnn7-wdata python2 tools/infer"
-            "_simple.py --cfg configs/DensePose_ResNet101_FPN_s1x-e2e.yaml --output-dir DensePoseData/output_results/"
-            " --image-ext jpg --wts DensePoseData/weights/weights.pkl DensePoseData/input_imgs/{}".format(
-                config['inference_dir'], img_name))
-        iuv = cv2.imread(os.path.join(config['output_dir'],img_name[:-4]+'_IUV.png'))
-        out = map_t.transfer_texture(im,iuv)
-        result_save_file=os.path.join(app.config['UPLOAD_FOLDER'],"texture_result.jpg")
-        io.imsave(result_save_file,out[...,::-1])
-        return send_from_directory(app.config['UPLOAD_FOLDER'],'texture_result.jpg', as_attachment=True)
+        video.save(saved_path)
+        process_video(saved_path,video_name)
+        return send_from_directory(config['send_from'],'texture_result.jpg', as_attachment=True)
     else:
         return "Where is the image?"
 
