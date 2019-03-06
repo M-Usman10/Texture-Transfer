95bcec7866e0559251aaec293f6573fed3d412a3
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-03-06 12:58:40 +0500

    Transfer for multi users by texture id

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def transfer_texture():
     app.logger.info(app.config['UPLOAD_FOLDER'])
     video = request.files['transfer']
     name = request.files['texture_filename']
-    map_t.texture_path = os.path.join(app.config['UPLOAD_FOLDER'], name)
+    map_t.texture_path = os.path.join(app.config['UPLOAD_FOLDER'], name + '.jpg')
     map_t.read_texture()
     video_name = secure_filename(video.filename)
     print ("Video/image to process is {}".format(video_name))
